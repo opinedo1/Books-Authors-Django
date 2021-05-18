dc893e9b0a0a747ebb0afa70b1501f9d7b88b8bc
Oscar Pinedo <opinedo19@gmail.com>
2021-05-18 13:52:35 -0700

    added notes field to Author model

--- books_authors/books_authors_app/models.py
+++ books_authors/books_authors_app/models.py
@@ -11,7 +11,7 @@ class Book(models.Model):
 class Author(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
+    notes = models.TextField(default="")
     books = models.ManyToManyField(Book, related_name="authors")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
