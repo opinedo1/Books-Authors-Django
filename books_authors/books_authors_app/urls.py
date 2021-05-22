fd0900832d535b514507cf4939fd147649ef9a08
Oscar Pinedo <opinedo19@gmail.com>
2021-05-22 16:59:58 -0700

    adds templates and urls for 2 page views, admin access setup

--- books_authors/books_authors_app/urls.py
+++ books_authors/books_authors_app/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    # path('add', views.add)
+    path('books/<int:book_id>', views.view_book)
 ]
--- books_authors/books_authors_app/views.py
+++ books_authors/books_authors_app/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render, HttpResponse
-# from .models import Author
+from django.shortcuts import render, HttpResponse, redirect
+from .models import Author, Book
 
 # Create your views here.
 def index(request):
-    # context = {
-    #     'authors': Author.objects.all()
-    # }
-    # return HttpResponse("Hello World")
-    return render(request, 'index.html')
+    context = {
+        'books': Book.objects.all()
+    }
+    return render(request, 'index.html', context)
+
+def view_book(request, book_id):
+    context = {
+        'books': Book.objects.all(),
+        'authors':Author.objects.all()
+    }
+    return render(request, 'book.html', context)
