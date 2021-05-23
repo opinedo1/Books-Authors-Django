bd43d48f1dc46ca66343175487650839d9479c85
Oscar Pinedo <opinedo19@gmail.com>
2021-05-22 18:57:32 -0700

    completed the authors main template, addition of new authors and ability to view specific authors from their respective links

--- books_authors/books_authors_app/urls.py
+++ books_authors/books_authors_app/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('', views.books),
     path('books/<int:book_id>', views.view_book),
     path('create_book', views.create_book),
-    path('authors', views.authors)
+    path('authors', views.authors),
+    path('authors/<int:author_id>', views.view_author),
+    path('create_author', views.create_author)
+
 ]
--- books_authors/books_authors_app/views.py
+++ books_authors/books_authors_app/views.py
@@ -9,7 +9,10 @@ def books(request):
     return render(request, 'books.html', context)
 
 def authors(request):
-    return render(request, 'authors.html', {"authors": Author.objects.all()})
+    context = {
+        'authors': Author.objects.all()
+    }
+    return render(request, 'authors.html', context)
 
 def create_book(request):
     Book.objects.create(
@@ -18,9 +21,24 @@ def create_book(request):
     )
     return redirect('/')
 
+def create_author(request):
+    Author.objects.create(
+        first_name = request.POST['first_name'],
+        last_name = request.POST['last_name'],
+        notes = request.POST['notes']
+    )
+    return redirect('/authors')
+
 def view_book(request, book_id):
     context = {
         'book': Book.objects.get(id=book_id),
         'authors': Book.objects.get(id=book_id).authors.all()
     }
     return render(request, 'book.html', context)
+
+def view_author(request, author_id):
+    context = {
+        'author': Author.objects.get(id=author_id),
+        'books': Author.objects.get(id=author_id).books.all()
+    }
+    return render(request, 'author.html', context)
