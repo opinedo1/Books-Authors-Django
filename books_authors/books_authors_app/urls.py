3b34d828afc94150818f96f1ce2a8f79a3b507d0
Oscar Pinedo <opinedo19@gmail.com>
2021-05-22 17:04:54 -0700

    renames views and templates to be relevant to its content

--- books_authors/books_authors_app/urls.py
+++ books_authors/books_authors_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.books),
     path('books/<int:book_id>', views.view_book)
 ]
--- books_authors/books_authors_app/views.py
+++ books_authors/books_authors_app/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render, HttpResponse, redirect
 from .models import Author, Book
 
 # Create your views here.
-def index(request):
+def books(request):
     context = {
         'books': Book.objects.all()
     }
-    return render(request, 'index.html', context)
+    return render(request, 'books.html', context)
 
 def view_book(request, book_id):
     context = {
