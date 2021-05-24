4924ed4849f88d87c86e5d367421172a5067fdb2
Oscar Pinedo <opinedo19@gmail.com>
2021-05-23 18:21:27 -0700

    can assign/add authors to individual books when viewing them

--- books_authors/books_authors_app/urls.py
+++ books_authors/books_authors_app/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     path('create_book', views.create_book),
     path('authors', views.authors),
     path('authors/<int:author_id>', views.view_author),
-    path('create_author', views.create_author)
-
+    path('create_author', views.create_author),
+    # Assigns books or authors to to the element being viewed
+    path('books/<int:book_id>/addauthor', views.addauthor),
+    path('authors/<int:author_id>/addbook', views.addbook),
+    
 ]
--- books_authors/books_authors_app/views.py
+++ books_authors/books_authors_app/views.py
@@ -32,13 +32,30 @@ def create_author(request):
 def view_book(request, book_id):
     context = {
         'book': Book.objects.get(id=book_id),
-        'authors': Book.objects.get(id=book_id).authors.all()
+        'authors': Book.objects.get(id=book_id).authors.all(),
+        # exclude authors who are already assigne to the book
+        'all_authors': Author.objects.exclude(books__id=book_id)
     }
     return render(request, 'book.html', context)
 
 def view_author(request, author_id):
     context = {
         'author': Author.objects.get(id=author_id),
-        'books': Author.objects.get(id=author_id).books.all()
+        'books': Author.objects.get(id=author_id).books.all(),
+        'all_books': Book.objects.exclude(authors__id=author_id)
     }
     return render(request, 'author.html', context)
+
+# assigns a book to an author
+def addauthor(request, book_id):
+    this_author = request.POST['author_id']
+    this_book = Book.objects.get(id=book_id)
+    this_book.authors.add(this_author)
+    return redirect('/authors/{}'.format(request.POST['author_id']))
+
+# assigns an author to a book
+def addbook(request, author_id):
+    print(request.POST)
+    this_book = request.POST['book_id']
+    this_author = Author.objects.get(id=author_id)
+    return redirect('/books/{}'.format(request.POST['book_id']))
