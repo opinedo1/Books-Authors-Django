00a231322d4749f0ae9e4c804d64d32de88a784a
Oscar Pinedo <opinedo19@gmail.com>
2021-05-23 18:26:28 -0700

    completed the backend route to assign/add authors to individual books

--- books_authors/books_authors_app/views.py
+++ books_authors/books_authors_app/views.py
@@ -46,16 +46,17 @@ def view_author(request, author_id):
     }
     return render(request, 'author.html', context)
 
-# assigns a book to an author
+# assigns an author to a book
 def addauthor(request, book_id):
     this_author = request.POST['author_id']
     this_book = Book.objects.get(id=book_id)
     this_book.authors.add(this_author)
     return redirect('/authors/{}'.format(request.POST['author_id']))
 
-# assigns an author to a book
+# assigns a book to an author
 def addbook(request, author_id):
     print(request.POST)
     this_book = request.POST['book_id']
     this_author = Author.objects.get(id=author_id)
+    this_author.books.add(this_book)
     return redirect('/books/{}'.format(request.POST['book_id']))
