08fee6b97c39d4eaf2b8a94e5da62117dbe3ab63
Oscar Pinedo <opinedo19@gmail.com>
2021-05-23 18:33:31 -0700

    cleaned up whitespace and added comments

--- books_authors/books_authors_app/urls.py
+++ books_authors/books_authors_app/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('authors', views.authors),
     path('authors/<int:author_id>', views.view_author),
     path('create_author', views.create_author),
-    # Assigns books or authors to to the element being viewed
+    # Views to assing books or authors to to the element being viewed
     path('books/<int:book_id>/addauthor', views.addauthor),
     path('authors/<int:author_id>/addbook', views.addbook),
     
